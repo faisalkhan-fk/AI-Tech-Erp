fca028efe8f1344b951044fead5fdb456302bacf
Faisal Khan <faisalkhan7863030@gmail.com>
2026-09-02 10:01:14 +0530

    Fix timezone issue by enforcing Asia/Kolkata IST

--- backend/src/main/java/com/aitech/erp/AiTechErpApplication.java
+++ backend/src/main/java/com/aitech/erp/AiTechErpApplication.java
@@ -3,8 +3,17 @@ package com.aitech.erp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class AiTechErpApplication {
+
+	@PostConstruct
+	public void init(){
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiTechErpApplication.class, args);
